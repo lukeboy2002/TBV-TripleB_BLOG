137689909773cf6f12f83f1b49962f258d706f7a
Lukeboy_2002 <35039280+lukeboy2002@users.noreply.github.com>
2024-06-26 18:12:40 +0200

    Configuration and addons
    Spatie Permissions
    Elequent-Sluggable
    Flowbite
    Authentication to username

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +16,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call(RolesAndPermissionsSeeder::class);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $admin = User::factory()
+//            ->has(Post::factory(45))
+//            ->has(Comment::factory(120)->recycle($posts))
+            ->create([
+                'username' => 'admin',
+                'email' => 'admin@test.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('adminadmin'),
+                'remember_token' => Str::random(10),
+            ]);
+        $role = Role::select('id')->where('name', 'admin')->first();
+        $admin->roles()->attach($role);
+
+        User::factory(10)->create();
+
+//        User::factory()->create([
+//            'name' => 'Test User',
+//            'email' => 'test@example.com',
+//        ]);
     }
 }
